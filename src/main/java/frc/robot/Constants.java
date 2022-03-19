e94d0876522750ccc36fba44d40c21db0d842f64
Warty <aniket.warty25@bcp.org>
2022-03-18 20:03:20 -0700

    Initial Commit + Flywheel

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,16 @@
+package frc.robot;
+
+public class Constants {
+    //Flywheel motor IDs
+    public static final int kFlywheelMasterId = 1;
+    public static final int kFlywheelFollowerId = 2;
+    public static final int kFlywheelEncoderId = 3;
+
+    //FLywheel PIDF
+    public static final double kFlywheelKp = 0;
+    public static final double kFlywheelKi = 0;
+    public static final double kFlywheelKd = 0;
+    public static final double kFlywheelKf = 0;
+    public static final int kSlotIdx = 0;
+    public static final int kFlywheelTimeoutMs = 1000;
+}
---
+++ src/main/java/frc/robot/subsystems/Flywheel.java
@@ -0,0 +1,54 @@
+package frc.robot.subsystems;
+import frc.robot.Constants;
+import frc.robot.PIDF;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+
+public class Flywheel extends Subsystem{
+    private static Flywheel mInstance;
+
+    public static Flywheel getInstance() {
+        if (mInstance == null) {
+            mInstance = new Flywheel();
+        }
+
+        return mInstance;
+    }
+
+    public class PeriodicIO {
+        double velocity_ticks = 0;
+        double velocity_demand = 0;
+    }
+
+    private PeriodicIO mPeriodicIO = new PeriodicIO();
+
+    private TalonFX mFlywheelMaster, mFlywheelFollower;
+    private TalonSRX mFlywheelEncoder = new TalonSRX(3);
+
+    public Flywheel(){
+        mFlywheelMaster = new TalonFX(Constants.kFlywheelMasterId);
+        mFlywheelFollower = new TalonFX(Constants.kFlywheelFollowerId);
+        mFlywheelEncoder.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Absolute, 0, 1000);
+        mFlywheelFollower.set(ControlMode.Follower, 1);
+        mFlywheelMaster.config_kP(Constants.kSlotIdx, Constants.kFlywheelKp, Constants.kFlywheelTimeoutMs);
+        mFlywheelMaster.config_kI(Constants.kSlotIdx, Constants.kFlywheelKi, Constants.kFlywheelTimeoutMs);
+        mFlywheelMaster.config_kD(Constants.kSlotIdx, Constants.kFlywheelKd, Constants.kFlywheelTimeoutMs);
+        mFlywheelMaster.config_kF(Constants.kSlotIdx, Constants.kFlywheelKf, Constants.kFlywheelTimeoutMs);
+    }
+
+    public void readPeriodicInput(){
+        mPeriodicIO.velocity_ticks = mFlywheelEncoder.getSelectedSensorPosition(0);
+    }
+
+    public void writePeriodicOutput(){
+        mFlywheelMaster.set(ControlMode.Velocity, mPeriodicIO.velocity_demand);
+    }
+
+    public void setRPM(double rpm){
+        mPeriodicIO.velocity_demand = rpm / 600;
+    }
+}
